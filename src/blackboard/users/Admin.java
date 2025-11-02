0c50cc5f355cb0258a45c39547db29fd682bdf94
Daniel <daguilar.ieu2023@student.ie.edu>
2025-11-02 13:44:57 +0100

    Finish base models for all users, fix admin base models

--- src/blackboard/users/Admin.java
+++ src/blackboard/users/Admin.java
@@ -1,17 +1,50 @@
 package blackboard.users;
 
+import blackboard.courses.Course;
+
 public class Admin extends User {
-    protected Admin(int id, String name, String username, String password, UserRole role) {
-        super(id, name, username, password, role);
+
+    // Base-models: fix role to ADMIN
+    public Admin(int id, String name, String username, String password) {
+        super(id, name, username, password, UserRole.ADMIN);
     }
 
     @Override
     public void save() throws Exception {
-
+        // TODO(feature/persistence-stub): persist admin record to storage
     }
 
     @Override
     public void load() throws Exception {
+        // TODO(feature/persistence-stub): load admin record from storage
+    }
 
+    public void createTeacher(Teacher t) {
+        // TODO(feature/base-models): store created teacher account
+    }
+
+    public void createStudent(Student s) {
+        // TODO(feature/base-models): store created student account
+    }
+
+    public void createCourse() {
+        // TODO(feature/base-models): create a new course record
+    }
+
+    public void enrollStudent(Course c) {
+        // TODO(feature/enrollment-rules): enroll a student into the course (choose student in UI)
+    }
+
+    public void assignTeacher(Course course, Teacher t) {
+        // TODO(feature/enrollment-rules): assign teacher to course
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", username='" + getUsername() + '\'' +
+                '}';
     }
 }
--- src/blackboard/users/Student.java
+++ src/blackboard/users/Student.java
@@ -1,17 +1,58 @@
 package blackboard.users;
 
+import blackboard.courses.Course;
+import blackboard.courses.Assignment;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Collections;
+
 public class Student extends User {
-    protected Student(int id, String name, String username, String password, UserRole role) {
-        super(id, name, username, password, role);
+    private final String studentId;
+
+    // Base-models: Student should fix its role to STUDENT
+    public Student(int id, String name, String username, String password, String studentId) {
+        super(id, name, username, password, UserRole.STUDENT);
+        this.studentId = studentId;
+    }
+
+    public String getStudentId() {
+        return studentId;
     }
 
     @Override
     public void save() throws Exception {
-
+        // TODO(feature/persistence-stub): persist student record to storage
     }
 
     @Override
     public void load() throws Exception {
+        // TODO(feature/persistence-stub): load student record from storage
+    }
 
+    /**
+     * View the list of courses the student is enrolled in.
+     */
+    public List<Course> viewEnrollments() {
+        // TODO(feature/enrollment-rules): fetch enrollments for this student and map to courses
+        return Collections.emptyList();
+    }
+
+    /**
+     * View grades for assignments in a given course.
+     */
+    public Map<Assignment, Double> viewGrades(Course course) {
+        // TODO(feature/grading): compute or aggregate assignment grades for this student in the given course
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", username='" + getUsername() + '\'' +
+                ", studentId='" + studentId + '\'' +
+                '}';
     }
 }
